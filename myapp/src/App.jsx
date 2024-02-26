e469ab1b8db17b802a829140776a2b1996fa48f2
YamamaAbuLiel <124617912+YamamaAbuLiel@users.noreply.github.com>
2024-02-26 23:30:28 +0200

    First react

---
+++ myapp/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+var name="Yamama Abu Liel";
+var age=22;
+ return(
+  <>
+  <h1> My name is {name} and I am {age} years old.</h1>
+  </>
+ )
+}
+
+export default App
